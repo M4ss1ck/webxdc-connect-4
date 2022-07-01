10e99ae74d669dd5c2be79bf4a5ab79c4f5aa22d
M4ss1ck <4ndyraul@gmail.com>
2022-06-30 23:19:07 -0400

    remove unused script

--- browser/src/app.ts
+++ browser/src/app.ts
@@ -13,41 +13,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const board = new Board(canvas)
   board.render()
 
-  const searchParams = new URLSearchParams(location.search)
-  const connectionMatchId = searchParams.get('matchId')
-  if (!!connectionMatchId) {
-    Game.initGameOnline2p()
-    const modeDOM = document.querySelector('.mode')
-    if (modeDOM) {
-      modeDOM.classList.add('hidden')
-    }
-  }
-
   const modeChooser = document.querySelector('.mode-chooser-submit')
   if (modeChooser) {
     modeChooser.addEventListener('click', () => {
       const modeDOM = document.querySelector('.mode')
       if (modeDOM) {
-        const modeInputDOMs = <NodeListOf<HTMLInputElement>>(
-          document.querySelectorAll('.mode-chooser-input')
-        )
-        let chosenMode = null
-        for (let i = 0; i < modeInputDOMs.length; i++) {
-          chosenMode = modeInputDOMs[i].checked ? modeInputDOMs[i].value : null
-          if (chosenMode) {
-            break
-          }
-        }
-        if (!chosenMode) {
-          chosenMode = 'offline-ai'
-        }
-        if (chosenMode === 'offline-human') {
-          Game.initGameLocal2p()
-        } else if (chosenMode === 'offline-ai') {
-          Game.initGameLocalAi()
-        } else if (chosenMode === 'online-human') {
-          Game.initGameOnline2p()
-        }
+        Game.initGameLocal2p()
 
         modeDOM.classList.add('invisible')
         modeDOM.addEventListener('transitionend', () => {
